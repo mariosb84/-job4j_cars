629b34f7e3240f1349438ca65718df969bf4e02b
Sergei Baranov <mariosb84@mail.ru>
2023-02-08 17:36:19 +0300

    2. Доменная модель проекта "АвтоМаг" [#504897]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,8 +1,13 @@
 package ru.job4j;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("123");
+        SpringApplication.run(Main.class, args);
+        System.out.println("Go to http://localhost:8080/cars");
     }
 
 }
